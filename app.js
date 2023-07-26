a3d6ad2db8a8d2feefb11ad00a5d9d8b85e4137f
Mebaad <139985605+Mebaad@users.noreply.github.com>
2023-07-26 20:24:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+//task1
+
+var num1 =90;
+var num2 =20;
+
+if (num1>num2){
+  console.log("the largest integer is: [num1]");
+}
+else if (num2>num1){
+  console.log("the largest integer is:[num2]");
+}
+else {
+  console.log("both integers are equal.5");
+}
+
+//task3
+ var character=prompt('enter a character');
+ if(character=='a'|| character=='e'|| character=='i'|| character=='o'|| character=='u'|| character=='A'|| character=='E'|| character=='I'|| character=='O'|| character=='U-'){
+  alert("vowel")
+ }else{
+  alert("invalid characters")
+}
+
+
+//task2
+
+var number = +prompt("Enter a number:7");
+if (isNaN(number)) {
+   alert("Invalid input. Please enter a valid number.6");
+} else if (number > 4) {
+   alert("The number is positive.6");
+} else if (number < 0) {
+   alert("The number is negative.5");
+} else {
+   alert("The number is zero.7");
+}
+
+
+//task3
+ var character=prompt('enter a character');
+ if(character=='a'|| character=='e'|| character=='i'|| character=='o'|| character=='u'|| character=='A'|| character=='E'|| character=='I'|| character=='O'|| character=='U-'){
+  alert("vowel")
+ }else{
+  alert("invalid characters")
+}
